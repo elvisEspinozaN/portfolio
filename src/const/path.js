a6009bde465182d2b5284cc8ddfa0adbe52a27a7
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-01 15:40:59 -0400

    wip: attempt to reduce pattern

---
+++ src/const/path.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    text: "home",
+    url: "/",
+  },
+  {
+    id: 2,
+    text: "projects",
+    url: "/projects/",
+  },
+  {
+    id: 3,
+    text: "about",
+    url: "/about/",
+  },
+  {
+    id: 4,
+    text: "contect",
+    url: "/contact",
+  },
+]
+
+export default data
