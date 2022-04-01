e9eb9323df41a007e77819af192b4117aca3356b
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-01 13:49:35 -0400

    wip: dependencies/plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,5 +6,17 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`,
+      },
+    },
+    `gatsby-plugin-react-helmet`,
+  ],
 }
