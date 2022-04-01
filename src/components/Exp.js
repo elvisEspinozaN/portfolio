a9fe3a0a62c751411dfa1da2af4a6904e269e892
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-01 15:38:14 -0400

    wip: basic components

---
+++ src/components/Exp.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Heading from "./Heading"
+import { Link } from "gatsby"
+
+const History = () => {
+  return <div>History</div>
+}
+
+export default History
---
+++ src/components/Projects.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const Projects = () => {
+  return <div>Projects</div>
+}
+
+export default Projects
