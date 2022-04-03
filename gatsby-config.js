7c756c1a0074d699e4e00c94368f5e0eaf218d2c
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-03 17:03:31 -0400

    wip: gatsby-browser -> integrates nav globally

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,16 @@ module.exports = {
       },
     },
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-strapi`,
+      options: {
+        apiURL: `http://localhost:1337`,
+        queryLimit: 1000, // Default to 100
+        contentTypes: [`experiences`],
+        //If using single types place them in this array.
+        singleTypes: [`about`],
+        // Possibility to login with a strapi user, when content types are not publically available (optional).
+      },
+    },
   ],
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react"
+import AccentPaths from "../const/accentPaths"
 
 const Footer = () => {
   return <div>Footer</div>
