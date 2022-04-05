c392712954d3e8d2a49b7ad34f85ec09f8ca66d2
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-05 17:42:42 -0400

    wip: 404 page completed

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,19 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Error = () => {
-  return <div>404</div>
+  return (
+    <>
+      <main className="error-main">
+        <div className="error-cont">
+          <h1>Page does not exist!</h1>
+          <Link to="/" className="btn">
+            Back Home
+          </Link>
+        </div>
+      </main>
+    </>
+  )
 }
 
 export default Error
