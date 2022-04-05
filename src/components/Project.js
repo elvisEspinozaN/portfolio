c26eca989756e483c83699e098acbe68b46d6ef1
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-05 11:24:30 -0400

    wip: project component finalized

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,13 +1,30 @@
 import React from "react"
-import { FaGithubSquar, FaSearch } from "react-icons/fa"
+import { FaGithubSquare, FaSearch } from "react-icons/fa"
 import { Link } from "gatsby"
 
-const Project = ({ title, desc, imgUrl, githubUrl, index, tags }) => {
+const Project = ({ title, desc, imgUrl, githubUrl, index, tags, url }) => {
   return (
     <article className="project">
       <img src={imgUrl} alt={title} className="project-img" />
       <div className="project-cont">
         <span className="project-num">0{index + 1}.</span>
+        <Link to={`/projects/${title}`}>
+          <h4>{title}</h4>
+        </Link>
+        <p className="project-desc">{desc}</p>
+        <div className="project tags">
+          {tags.map(tag => {
+            return <span key={tag.id}>{tag.tag}</span>
+          })}
+        </div>
+        <div className="project-link">
+          <a href={githubUrl}>
+            <FaGithubSquare className="project-icon"></FaGithubSquare>
+          </a>
+          <a href={url}>
+            <FaSearch className="project-icon"></FaSearch>
+          </a>
+        </div>
       </div>
     </article>
   )
