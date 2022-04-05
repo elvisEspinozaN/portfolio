0d91e5566c495cbfc092f1524d1295957855a4ab
Elvis Espinoza Navarrete <elvis.espinoza.navarrete@outlook.com>
2022-04-05 17:03:52 -0400

    wip: all projects page done (to be worked on)

--- src/components/Allp.js
+++ src/components/Allp.js
@@ -1,31 +1,19 @@
 import React from "react"
-import { FaGithubSquare, FaSearch } from "react-icons/fa"
+import Heading from "./Heading"
+import Project from "./Project"
+import allP from "../const/allP"
 import { Link } from "gatsby"
 
-const Allp = ({ title, desc, imgUrl, githubUrl, tags, url }) => {
+const Allp = () => {
   return (
-    <article className="project">
-      <img src={imgUrl} alt={title} className="project-img" />
-      <div className="project-cont">
-        <Link className="project-page" to="/projects">
-          <h4>{title}</h4>
-        </Link>
-        <p className="project-desc">{desc}</p>
-        <div className="project-tags">
-          {tags.map(tag => {
-            return <span key={tag.id}>{tag.tag}</span>
-          })}
-        </div>
-        <div className="project-link">
-          <a href={githubUrl}>
-            <FaGithubSquare className="project-icon"></FaGithubSquare>
-          </a>
-          <a href={url}>
-            <FaSearch className="project-icon"></FaSearch>
-          </a>
-        </div>
+    <section className="section featured">
+      <Heading heading="My Projects" />
+      <div className="section-center featured-center">
+        {allP.map((all, e) => {
+          return <Project key={all.id} index={e} {...all} />
+        })}
       </div>
-    </article>
+    </section>
   )
 }
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,17 @@
 import React from "react"
+import Allp from "../components/Allp"
+import allP from "../const/allP"
 
 const ProjectsPage = () => {
-  return
+  return (
+    <>
+      <main>
+        <section className="projects-main">
+          <Allp />
+        </section>
+      </main>
+    </>
+  )
 }
 
 export default ProjectsPage
